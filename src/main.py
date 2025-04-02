5a9fc12da35001e90069ecf98847e1192f60b655
Pavel <sol.pablo2015@yandex.ru>
2025-04-02 11:17:55 +0300

    fix START_COMMAND

--- src/main.py
+++ src/main.py
@@ -7,19 +7,13 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv("TG_BOT_TOKEN")
-
-if not TOKEN:
-    raise ValueError("The token was not found! Check .env file")
-
-bot = Bot(TOKEN)
-dp = Dispatcher()
 
 HELP_COMMAND = """
 /start - начать работу с ботом
 /help - список команд
 """
+
+START_COMMAND = "Добро пожаловать в бот для ленивых АСВКшников!"
 
 
 async def help_command(message: types.Message):
@@ -29,11 +23,20 @@ async def help_command(message: types.Message):
 
 async def start_command(message: types.Message):
     """Answer command start."""
-    await message.answer(text="Добро пожаловать в бот для ленивых АСВКшников!")
+    await message.answer(text=START_COMMAND)
 
 
 async def main():
     """Start bot and print logs."""
+    load_dotenv()
+    TOKEN = os.getenv("TG_BOT_TOKEN")
+
+    if not TOKEN:
+        raise ValueError("The token was not found! Check .env file")
+
+    bot = Bot(TOKEN)
+    dp = Dispatcher()
+
     logging.basicConfig(level=logging.INFO)
     dp.message.register(start_command, Command("start"))
     dp.message.register(help_command, Command("help"))
