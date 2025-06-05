1f2d3156c0355c067a5ca843a285f1872947eb96
Pavel <sol.pablo2015@yandex.ru>
2025-06-05 17:31:17 +0300

    moved handlers to a separate file

---
+++ handlers.py
@@ -0,0 +1,80 @@
+"""Handlers for our bot."""
+import supabase as sb
+
+from config_reader import config
+
+from aiogram import F, Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message, CallbackQuery
+from aiogram.types.inline_keyboard_button import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
+
+router = Router()
+
+URL = config.url.get_secret_value()
+KEY = config.key.get_secret_value()
+
+CLIENT = sb.create_client(URL, KEY)
+
+
+class Registration(StatesGroup):
+    """Fields to be complited during registartion."""
+
+    name = State()
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message, state: FSMContext) -> None:
+    """Greeting of the bot."""
+    registration = InlineKeyboardBuilder()
+    registration.add(InlineKeyboardButton(
+        text="Регистрация",
+        callback_data="registration")
+    )
+    await message.answer("Привет! Я бот 321 группы. Для начала необходимо зарегестрироваться.",
+                         reply_markup=registration.as_markup())
+
+
+@router.callback_query(F.data == 'registration')
+async def registration(callback: CallbackQuery, state: FSMContext):
+    """Start of registartion."""
+    same_user = CLIENT.table("users").select("*").eq("tg_id", str(callback.from_user.id)).execute().data
+    if same_user:
+        check = InlineKeyboardBuilder()
+        check.add(InlineKeyboardButton(
+            text="Всё верно",
+            callback_data='wait'
+        ))
+        check.add(InlineKeyboardButton(
+            text='Редактировать',
+            callback_data='fix'
+        ))
+        await callback.message.answer(f"Вы уже зарегестрированы со следующими данными.\n\nФИО: {same_user[0]['name']}",
+                                      reply_markup=check.as_markup())
+        return
+    await callback.message.answer('Введите ваше ФИО:')
+    await state.set_state(Registration.name)
+
+
+@router.callback_query(F.data == 'fix')
+async def fix_registration(callback: CallbackQuery, state: FSMContext):
+    """Start registration from the beginning."""
+    CLIENT.table("users").delete().eq("tg_id", str(callback.from_user.id)).execute()
+    await callback.message.answer('Введите ваше ФИО:')
+    await state.set_state(Registration.name)
+
+
+@router.message(F.text, Registration.name)
+async def process_name(message: Message, state: FSMContext):
+    """Add user."""
+    CLIENT.table("users").insert({"tg_id": str(message.from_user.id), "name": message.text}).execute()
+    await message.answer("Отлично, дождитесь решения админимтратора.")
+
+
+@router.callback_query(F.data == 'wait')
+async def wait(callback: CallbackQuery, state: FSMContext):
+    """Standart response."""
+    await callback.message.answer('Отлично!')
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import asyncio
 
 from config_reader import config
+from handlers import router
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
@@ -18,78 +19,16 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
 TOKEN = config.bot_token.get_secret_value()
-URL = config.url.get_secret_value()
-KEY = config.key.get_secret_value()
+
 
 STORAGE = MemoryStorage()
 dp = Dispatcher(storage=STORAGE)
 BOT = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-CLIENT = sb.create_client(URL, KEY)
-
-
-class Registration(StatesGroup):
-    """Fields to be complited during registartion."""
-
-    name = State()
-
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message, state: FSMContext) -> None:
-    """Greeting of the bot."""
-    registration = InlineKeyboardBuilder()
-    registration.add(InlineKeyboardButton(
-        text="Регистрация",
-        callback_data="registration")
-    )
-    await message.answer("Привет! Я бот 321 группы. Для начала необходимо зарегестрироваться.",
-                         reply_markup=registration.as_markup())
-
-
-@dp.callback_query(F.data == 'registration')
-async def registration(callback: CallbackQuery, state: FSMContext):
-    """Start of registartion."""
-    same_user = CLIENT.table("users").select("*").eq("tg_id", str(callback.from_user.id)).execute().data
-    if same_user:
-        check = InlineKeyboardBuilder()
-        check.add(InlineKeyboardButton(
-            text="Всё верно",
-            callback_data='wait'
-        ))
-        check.add(InlineKeyboardButton(
-            text='Редактировать',
-            callback_data='fix'
-        ))
-        await callback.message.answer(f"Вы уже зарегестрированы со следующими данными.\n\nФИО: {same_user[0]['name']}",
-                                      reply_markup=check.as_markup())
-        return
-    await callback.message.answer('Введите ваше ФИО:')
-    await state.set_state(Registration.name)
-
-
-@dp.callback_query(F.data == 'fix')
-async def fix_registration(callback: CallbackQuery, state: FSMContext):
-    """Start registration from the beginning."""
-    CLIENT.table("users").delete().eq("tg_id", str(callback.from_user.id)).execute()
-    await callback.message.answer('Введите ваше ФИО:')
-    await state.set_state(Registration.name)
-
-
-@dp.message(F.text, Registration.name)
-async def process_name(message: Message, state: FSMContext):
-    """Add user."""
-    CLIENT.table("users").insert({"tg_id": str(message.from_user.id), "name": message.text}).execute()
-    await message.answer("Отлично, дождитесь решения админимтратора.")
-
-
-@dp.callback_query(F.data == 'wait')
-async def wait(callback: CallbackQuery, state: FSMContext):
-    """Standart response."""
-    await callback.message.answer('Отлично!')
-
 
 async def main() -> None:
     """Run bot."""
+    dp.include_router(router)
     await dp.start_polling(BOT)
 
 if __name__ == "__main__":
